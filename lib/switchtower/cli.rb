e7b3af7f5a88050362198737931a9ccfc794c589
Jamis Buck <jamis@37signals.com>
2005-08-31 15:54:33 +0000

    SwitchTower: incorporate deployment generator for simple Rails integration
    git-svn-id: http://svn.rubyonrails.org/rails/tools/switchtower@2084 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- lib/switchtower/cli.rb
+++ lib/switchtower/cli.rb
@@ -32,12 +32,17 @@ module SwitchTower
     end
 
     attr_reader :options
+    attr_reader :args
 
-    def initialize
+    def initialize(args = ARGV)
+      @args = args
       @options = { :verbose => 0, :recipes => [], :actions => [], :vars => {} }
 
       OptionParser.new do |opts|
-        opts.banner = "Usage: #{$0} [options]"
+        opts.banner = "Usage: #{$0} [options] [args]"
+
+        opts.separator ""
+        opts.separator "Recipe Options -----------------------"
         opts.separator ""
 
         opts.on("-a", "--action ACTION",
@@ -49,14 +54,6 @@ module SwitchTower
           "The password to use when connecting.",
           "(Default: prompt for password)"
         ) { |value| @options[:password] = value }
-
-        opts.on("-P", "--[no-]pretend",
-          "Run the task(s), but don't actually connect to or",
-          "execute anything on the servers. (For various reasons",
-          "this will not necessarily be an accurate depiction",
-          "of the work that will actually be performed.",
-          "Default: don't pretend.)"
-        ) { |value| @options[:pretend] = value }
 
         opts.on("-r", "--recipe RECIPE",
           "A recipe file to load. Multiple recipes may",
@@ -71,17 +68,39 @@ module SwitchTower
           @options[:vars][name.to_sym] = value
         end
 
+        opts.separator ""
+        opts.separator "Framework Integration Options --------"
+        opts.separator ""
+
+        opts.on("-A", "--apply-to [DIRECTORY]",
+          "Create a minimal set of scripts and recipes to use",
+          "switchtower with the application at the given",
+          "directory. (Currently only works with Rails apps.)"
+        ) { |value| @options[:apply_to] = value }
+
+        opts.separator ""
+        opts.separator "Miscellaneous Options ----------------"
+        opts.separator ""
+
+        opts.on("-h", "--help", "Display this help message") do
+          puts opts
+          exit
+        end
+
+        opts.on("-P", "--[no-]pretend",
+          "Run the task(s), but don't actually connect to or",
+          "execute anything on the servers. (For various reasons",
+          "this will not necessarily be an accurate depiction",
+          "of the work that will actually be performed.",
+          "Default: don't pretend.)"
+        ) { |value| @options[:pretend] = value }
+
         opts.on("-v", "--verbose",
           "Specify the verbosity of the output.",
           "May be given multiple times. (Default: silent)"
         ) { @options[:verbose] += 1 }
 
-        opts.separator ""
-        opts.on_tail("-h", "--help", "Display this help message") do
-          puts opts
-          exit
-        end
-        opts.on_tail("-V", "--version",
+        opts.on("-V", "--version",
           "Display the version info for this utility"
         ) do
           require 'switchtower/version'
@@ -89,11 +108,25 @@ module SwitchTower
           exit
         end
 
-        opts.parse!
+        opts.separator ""
+        opts.separator <<DETAIL.split(/\n/)
+You can use the --apply-to switch to generate a minimal set of switchtower
+scripts and recipes for an application. Just specify the path to the application
+as the argument to --apply-to, and then specify the name of your application,
+like this:
+
+  switchtower --apply-to ~/projects/myapp MyApplication
+
+You'll wind up with a sample deployment recipe in config/deploy.rb, some new
+rake tasks in config/tasks, and a switchtower script in your script directory.
+
+(Currently, --apply-to only works with Rails applications.)
+DETAIL
+
+        opts.parse!(args)
       end
 
-      abort "You must specify at least one recipe" if @options[:recipes].empty?
-      abort "You must specify at least one action" if @options[:actions].empty?
+      check_options!
 
       unless @options.has_key?(:password)
         @options[:password] = Proc.new do
@@ -113,18 +146,55 @@ module SwitchTower
     end
 
     def execute!
-      config = SwitchTower::Configuration.new
-      config.logger.level = options[:verbose]
-      config.set :password, options[:password]
-      config.set :pretend, options[:pretend]
-
-      config.load "standard" # load the standard recipe definition
-
-      options[:recipes].each { |recipe| config.load(recipe) }
-      options[:vars].each { |name, value| config.set(name, value) }
-
-      actor = config.actor
-      options[:actions].each { |action| actor.send action }
+      if !@options[:recipes].empty?
+        execute_recipes!
+      elsif @options[:apply_to]
+        execute_apply_to!
+      end
     end
+
+    private
+
+      def execute_recipes!
+        config = SwitchTower::Configuration.new
+        config.logger.level = options[:verbose]
+        config.set :password, options[:password]
+        config.set :pretend, options[:pretend]
+
+        config.load "standard" # load the standard recipe definition
+
+        options[:recipes].each { |recipe| config.load(recipe) }
+        options[:vars].each { |name, value| config.set(name, value) }
+
+        actor = config.actor
+        options[:actions].each { |action| actor.send action }
+      end
+
+      def execute_apply_to!
+        require 'switchtower/generators/rails/loader'
+        Generators::RailsLoader.load! @options
+      end
+
+      APPLY_TO_OPTIONS = [:apply_to]
+      RECIPE_OPTIONS   = [:password]
+
+      def check_options!
+        apply_to_given = !(@options.keys & APPLY_TO_OPTIONS).empty?
+        recipe_given   = !(@options.keys & RECIPE_OPTIONS).empty? ||
+                         !@options[:recipes].empty? ||
+                         !@options[:actions].empty?
+
+        if apply_to_given && recipe_given
+          abort "You cannot specify both recipe options and framework integration options."
+        elsif !apply_to_given
+          abort "You must specify at least one recipe" if @options[:recipes].empty?
+          abort "You must specify at least one action" if @options[:actions].empty?
+        else
+          @options[:application] = args.shift
+          @options[:recipe_file] = args.shift
+
+          abort "You must specify the name of the application" if @options[:application].nil?
+        end
+      end
   end
 end
---
+++ lib/switchtower/generators/rails/loader.rb
@@ -0,0 +1,20 @@
+module SwitchTower
+  module Generators
+    class RailsLoader
+      def self.load!(options)
+        require "#{options[:apply_to]}/config/environment"
+        require "rails_generator"
+        require "rails_generator/scripts/generate"
+
+        Rails::Generator::Base.sources << Rails::Generator::PathSource.new(
+          :switchtower, File.dirname(__FILE__))
+
+        args = ["deployment"]
+        args << options[:application] || "Application"
+        args << options[:recipe_file] || "deploy"
+
+        Rails::Generator::Scripts::Generate.new.run(args)
+      end
+    end
+  end
+end
