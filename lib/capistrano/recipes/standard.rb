d415067077ac76fd9af6a818e5811094cc32995b
Jamis Buck <jamis@37signals.com>
2007-03-30 18:46:40 +0000

    Make the task description parser a bit smarter, so that heredocs can be used for task descriptions again
    git-svn-id: http://svn.rubyonrails.org/rails/tools/capistrano@6495 5ecf4fe2-1ee6-0310-87b1-e25e094e27de

--- lib/capistrano/recipes/standard.rb
+++ lib/capistrano/recipes/standard.rb
@@ -1,18 +1,18 @@
 desc <<-DESC
-Invoke a single command on the remote servers. This is useful for performing \
-one-off commands that may not require a full task to be written for them. \
-Simply specify the command to execute via the COMMAND environment variable. \
-To execute the command only on certain roles, specify the ROLES environment \
-variable as a comma-delimited list of role names. Alternatively, you can \
-specify the HOSTS environment variable as a comma-delimited list of hostnames \
-to execute the task on those hosts, explicitly. Lastly, if you want to \
-execute the command via sudo, specify a non-empty value for the SUDO \
-environment variable.
+  Invoke a single command on the remote servers. This is useful for performing \
+  one-off commands that may not require a full task to be written for them. \
+  Simply specify the command to execute via the COMMAND environment variable. \
+  To execute the command only on certain roles, specify the ROLES environment \
+  variable as a comma-delimited list of role names. Alternatively, you can \
+  specify the HOSTS environment variable as a comma-delimited list of hostnames \
+  to execute the task on those hosts, explicitly. Lastly, if you want to \
+  execute the command via sudo, specify a non-empty value for the SUDO \
+  environment variable.
 
-Sample usage:
+  Sample usage:
 
-  $ cap COMMAND=uptime HOSTS=foo.capistano.test invoke
-  $ cap ROLES=app,web SUDO=1 COMMAND="tail -f /var/log/messages" invoke
+    $ cap COMMAND=uptime HOSTS=foo.capistano.test invoke
+    $ cap ROLES=app,web SUDO=1 COMMAND="tail -f /var/log/messages" invoke
 DESC
 task :invoke do
   method = ENV["SUDO"] ? :sudo : :run
@@ -20,14 +20,14 @@ task :invoke do
 end
 
 desc <<-DESC
-Begin an interactive Capistrano session. This gives you an interactive \
-terminal from which to execute tasks and commands on all of your servers. \
-(This is still an experimental feature, and is subject to change without \
-notice!)
+  Begin an interactive Capistrano session. This gives you an interactive \
+  terminal from which to execute tasks and commands on all of your servers. \
+  (This is still an experimental feature, and is subject to change without \
+  notice!)
 
-Sample usage:
+  Sample usage:
 
-  $ cap shell
+    $ cap shell
 DESC
 task :shell do
   require 'capistrano/shell'
