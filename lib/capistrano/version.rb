f00b84aad57c0a09c9c3a3b2b5a6d9fac5cbf298
Jamis Buck <jamis@37signals.com>
2008-11-07 13:56:46 -0700

    version bump

--- lib/capistrano/version.rb
+++ lib/capistrano/version.rb
@@ -6,7 +6,7 @@ module Capistrano
   class Version < Net::SSH::Version
     MAJOR = 2
     MINOR = 5
-    TINY  = 0
+    TINY  = 1
 
     # The current version, as a Version instance
     CURRENT = new(MAJOR, MINOR, TINY)
