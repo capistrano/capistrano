9431478cc59206f042a581704ecb6f0585d02c3a
Lee Hambley <lee.hambley@gmail.com>
2009-04-21 19:33:07 +0100

    Copies the flexible versioned deploy file from http://bit.ly/Wj9jA

--- lib/capistrano/recipes/deploy.rb
+++ lib/capistrano/recipes/deploy.rb
@@ -151,7 +151,7 @@ namespace :deploy do
   DESC
   task :default do
     update
-    restart
+    # restart
   end
 
   desc <<-DESC
@@ -287,20 +287,20 @@ namespace :deploy do
     files.each { |file| top.upload(file, File.join(current_path, file)) }
   end
 
-  desc <<-DESC
-    Restarts your application. This works by calling the script/process/reaper \
-    script under the current path.
-    
-    By default, this will be invoked via sudo as the `app' user. If \
-    you wish to run it as a different user, set the :runner variable to \
-    that user. If you are in an environment where you can't use sudo, set \
-    the :use_sudo variable to false:
-    
-      set :use_sudo, false
-  DESC
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    try_runner "#{current_path}/script/process/reaper"
-  end
+  # desc <<-DESC
+  #   Restarts your application. This works by calling the script/process/reaper \
+  #   script under the current path.
+  #   
+  #   By default, this will be invoked via sudo as the `app' user. If \
+  #   you wish to run it as a different user, set the :runner variable to \
+  #   that user. If you are in an environment where you can't use sudo, set \
+  #   the :use_sudo variable to false:
+  #   
+  #     set :use_sudo, false
+  # DESC
+  # task :restart, :roles => :app, :except => { :no_release => true } do
+  #   try_runner "#{current_path}/script/process/reaper"
+  # end
 
   namespace :rollback do
     desc <<-DESC
@@ -348,50 +348,50 @@ namespace :deploy do
     end
   end
 
-  desc <<-DESC
-    Run the migrate rake task. By default, it runs this in most recently \
-    deployed version of the app. However, you can specify a different release \
-    via the migrate_target variable, which must be one of :latest (for the \
-    default behavior), or :current (for the release indicated by the \
-    `current' symlink). Strings will work for those values instead of symbols, \
-    too. You can also specify additional environment variables to pass to rake \
-    via the migrate_env variable. Finally, you can specify the full path to the \
-    rake executable by setting the rake variable. The defaults are:
+  # desc <<-DESC
+  #   Run the migrate rake task. By default, it runs this in most recently \
+  #   deployed version of the app. However, you can specify a different release \
+  #   via the migrate_target variable, which must be one of :latest (for the \
+  #   default behavior), or :current (for the release indicated by the \
+  #   `current' symlink). Strings will work for those values instead of symbols, \
+  #   too. You can also specify additional environment variables to pass to rake \
+  #   via the migrate_env variable. Finally, you can specify the full path to the \
+  #   rake executable by setting the rake variable. The defaults are:
+  # 
+  #     set :rake,           "rake"
+  #     set :rails_env,      "production"
+  #     set :migrate_env,    ""
+  #     set :migrate_target, :latest
+  # DESC
+  # task :migrate, :roles => :db, :only => { :primary => true } do
+  #   rake = fetch(:rake, "rake")
+  #   rails_env = fetch(:rails_env, "production")
+  #   migrate_env = fetch(:migrate_env, "")
+  #   migrate_target = fetch(:migrate_target, :latest)
+  # 
+  #   directory = case migrate_target.to_sym
+  #     when :current then current_path
+  #     when :latest  then current_release
+  #     else raise ArgumentError, "unknown migration target #{migrate_target.inspect}"
+  #     end
+  # 
+  #   run "cd #{directory}; #{rake} RAILS_ENV=#{rails_env} #{migrate_env} db:migrate"
+  # end
 
-      set :rake,           "rake"
-      set :rails_env,      "production"
-      set :migrate_env,    ""
-      set :migrate_target, :latest
-  DESC
-  task :migrate, :roles => :db, :only => { :primary => true } do
-    rake = fetch(:rake, "rake")
-    rails_env = fetch(:rails_env, "production")
-    migrate_env = fetch(:migrate_env, "")
-    migrate_target = fetch(:migrate_target, :latest)
-
-    directory = case migrate_target.to_sym
-      when :current then current_path
-      when :latest  then current_release
-      else raise ArgumentError, "unknown migration target #{migrate_target.inspect}"
-      end
-
-    run "cd #{directory}; #{rake} RAILS_ENV=#{rails_env} #{migrate_env} db:migrate"
-  end
-
-  desc <<-DESC
-    Deploy and run pending migrations. This will work similarly to the \
-    `deploy' task, but will also run any pending migrations (via the \
-    `deploy:migrate' task) prior to updating the symlink. Note that the \
-    update in this case it is not atomic, and transactions are not used, \
-    because migrations are not guaranteed to be reversible.
-  DESC
-  task :migrations do
-    set :migrate_target, :latest
-    update_code
-    migrate
-    symlink
-    restart
-  end
+  # desc <<-DESC
+  #   Deploy and run pending migrations. This will work similarly to the \
+  #   `deploy' task, but will also run any pending migrations (via the \
+  #   `deploy:migrate' task) prior to updating the symlink. Note that the \
+  #   update in this case it is not atomic, and transactions are not used, \
+  #   because migrations are not guaranteed to be reversible.
+  # DESC
+  # task :migrations do
+  #   set :migrate_target, :latest
+  #   update_code
+  #   migrate
+  #   symlink
+  #   restart
+  # end
 
   desc <<-DESC
     Clean up old releases. By default, the last 5 releases are kept on each \
@@ -462,41 +462,41 @@ namespace :deploy do
   DESC
   task :cold do
     update
-    migrate
+    # migrate
     start
   end
 
-  desc <<-DESC
-    Start the application servers. This will attempt to invoke a script \
-    in your application called `script/spin', which must know how to start \
-    your application listeners. For Rails applications, you might just have \
-    that script invoke `script/process/spawner' with the appropriate \
-    arguments.
-
-    By default, the script will be executed via sudo as the `app' user. If \
-    you wish to run it as a different user, set the :runner variable to \
-    that user. If you are in an environment where you can't use sudo, set \
-    the :use_sudo variable to false.
-  DESC
-  task :start, :roles => :app do
-    run "cd #{current_path} && #{try_runner} nohup script/spin"
-  end
-
-  desc <<-DESC
-    Stop the application servers. This will call script/process/reaper for \
-    both the spawner process, and all of the application processes it has \
-    spawned. As such, it is fairly Rails specific and may need to be \
-    overridden for other systems.
-
-    By default, the script will be executed via sudo as the `app' user. If \
-    you wish to run it as a different user, set the :runner variable to \
-    that user. If you are in an environment where you can't use sudo, set \
-    the :use_sudo variable to false.
-  DESC
-  task :stop, :roles => :app do
-    run "if [ -f #{current_path}/tmp/pids/dispatch.spawner.pid ]; then #{try_runner} #{current_path}/script/process/reaper -a kill -r dispatch.spawner.pid; fi"
-    try_runner "#{current_path}/script/process/reaper -a kill"
-  end
+  # desc <<-DESC
+  #   Start the application servers. This will attempt to invoke a script \
+  #   in your application called `script/spin', which must know how to start \
+  #   your application listeners. For Rails applications, you might just have \
+  #   that script invoke `script/process/spawner' with the appropriate \
+  #   arguments.
+  # 
+  #   By default, the script will be executed via sudo as the `app' user. If \
+  #   you wish to run it as a different user, set the :runner variable to \
+  #   that user. If you are in an environment where you can't use sudo, set \
+  #   the :use_sudo variable to false.
+  # DESC
+  # task :start, :roles => :app do
+  #   run "cd #{current_path} && #{try_runner} nohup script/spin"
+  # end
+  # 
+  # desc <<-DESC
+  #   Stop the application servers. This will call script/process/reaper for \
+  #   both the spawner process, and all of the application processes it has \
+  #   spawned. As such, it is fairly Rails specific and may need to be \
+  #   overridden for other systems.
+  # 
+  #   By default, the script will be executed via sudo as the `app' user. If \
+  #   you wish to run it as a different user, set the :runner variable to \
+  #   that user. If you are in an environment where you can't use sudo, set \
+  #   the :use_sudo variable to false.
+  # DESC
+  # task :stop, :roles => :app do
+  #   run "if [ -f #{current_path}/tmp/pids/dispatch.spawner.pid ]; then #{try_runner} #{current_path}/script/process/reaper -a kill -r dispatch.spawner.pid; fi"
+  #   try_runner "#{current_path}/script/process/reaper -a kill"
+  # end
 
   namespace :pending do
     desc <<-DESC
