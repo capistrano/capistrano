6f77328c544b62fcbe1d5010ca5a8ea02b5296dd
Lee Hambley <lee.hambley@gmail.com>
2009-05-26 19:53:36 +0100

    More tweaks, getting somewhere now

--- lib/capistrano/recipes/deploy.rb
+++ lib/capistrano/recipes/deploy.rb
@@ -179,7 +179,9 @@ namespace :deploy do
     dirs = [deploy_to, releases_path, shared_path]
     dirs += shared_children.map { |d| File.join(shared_path, d) }
     run "#{try_sudo} mkdir -p #{dirs.join(' ')} && #{try_sudo} chmod g+w #{dirs.join(' ')}"
-    run "#{try_sudo} chown -R #{user}:#{user} #{dirs.join(' ')}"
+    unless user.nil?
+      run "#{try_sudo} chown -R #{user}:#{user} #{dirs.join(' ')}"
+    end
   end
 
   desc <<-DESC
