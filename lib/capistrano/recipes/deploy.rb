capistrano/capistrano

commit 92941e855d70a5778eb094a972fdf43c70802b95

Altered the `ls` command used to list older releases, should fix #88 for the majority of use cases; have documented the other options for people who need to customise this via the :release_name functionality.

lib/capistrano/recipes/deploy.rb
@@ -38,6 +38,7 @@ _cset(:real_revision)     { source.local.query_revision(revision) { |cmd| with_e
 
 _cset(:strategy)          { Capistrano::Deploy::Strategy.new(deploy_via, self) }
 
+# If overriding release name, please also select an appropriate setting for :releases below.
 _cset(:release_name)      { set :deploy_timestamped, true; Time.now.utc.strftime("%Y%m%d%H%M%S") }
 
 _cset :version_dir,       "releases"
@@ -50,7 +51,7 @@ _cset(:shared_path)       { File.join(deploy_to, shared_dir) }
 _cset(:current_path)      { File.join(deploy_to, current_dir) }
 _cset(:release_path)      { File.join(releases_path, release_name) }
 
-_cset(:releases)          { capture("ls -xt #{releases_path}").split.reverse }
+_cset(:releases)          { capture("ls -x #{releases_path}").split.reverse }
 _cset(:current_release)   { File.join(releases_path, releases.last) }
 _cset(:previous_release)  { releases.length > 1 ? File.join(releases_path, releases[-2]) : nil }
 

commit 8638f2278ebe484ebcb1a1950e157c30c2d70960

Re: #88, remove system inconsistencies using ls
The fix committed in 92941e855 broke me, I think if there are really
inconsistencies between systems in how ls sorts, then we should simply
allow ruby to sort it.

lib/capistrano/recipes/deploy.rb
@@ -51,7 +51,9 @@ _cset(:shared_path)       { File.join(deploy_to, shared_dir) }
 _cset(:current_path)      { File.join(deploy_to, current_dir) }
 _cset(:release_path)      { File.join(releases_path, release_name) }
 
-_cset(:releases)          { capture("ls -x #{releases_path}").split.reverse }
+# re: https://capistrano.lighthouseapp.com/projects/8716/tickets/88-getting-the-newest-directory
+# remove system inconsistencies with ls and let ruby sort the releases
+_cset(:releases)          { capture("ls -x #{releases_path}").split.sort }
 _cset(:current_release)   { File.join(releases_path, releases.last) }
 _cset(:previous_release)  { releases.length > 1 ? File.join(releases_path, releases[-2]) : nil }
 
@@ -386,6 +388,7 @@ namespace :deploy do
       else raise ArgumentError, "unknown migration target #{migrate_target.inspect}"
       end
 
+    puts "#{migrate_target} => #{directory}"
     run "cd #{directory}; #{rake} RAILS_ENV=#{rails_env} #{migrate_env} db:migrate"
   end
 