07558caa56744c58c92dad081cedfea21d6ba546
Chris Griego <cgriego@gmail.com>
2010-01-11 12:12:00 -0600

    Parameterize the asset paths used when normalizing timestamps using a :public_children variable.

--- lib/capistrano/recipes/deploy.rb
+++ lib/capistrano/recipes/deploy.rb
@@ -224,7 +224,8 @@ namespace :deploy do
     public/stylesheets, and public/javascripts so that the times are \
     consistent (so that asset timestamping works).  This touch process \
     is only carried out if the :normalize_asset_timestamps variable is \
-    set to true, which is the default.
+    set to true, which is the default The asset directories can be overridden \
+    using the :public_children variable.
   DESC
   task :finalize_update, :except => { :no_release => true } do
     run "chmod -R g+w #{latest_release}" if fetch(:group_writable, true)
@@ -242,7 +243,7 @@ namespace :deploy do
 
     if fetch(:normalize_asset_timestamps, true)
       stamp = Time.now.utc.strftime("%Y%m%d%H%M.%S")
-      asset_paths = %w(images stylesheets javascripts).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
+      asset_paths = fetch(:public_children, %w(images stylesheets javascripts)).map { |p| "#{latest_release}/public/#{p}" }.join(" ")
       run "find #{asset_paths} -exec touch -t #{stamp} {} ';'; true", :env => { "TZ" => "UTC" }
     end
   end
