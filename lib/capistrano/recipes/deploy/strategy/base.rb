20b168df068285558512101084af773db83ff0e0
Chris Griego <cgriego@gmail.com>
2010-01-11 11:58:39 -0600

    Added elapsed time to remote and local command finished log messages

--- lib/capistrano/recipes/deploy/strategy/base.rb
+++ lib/capistrano/recipes/deploy/strategy/base.rb
@@ -1,3 +1,4 @@
+require 'benchmark'
 require 'capistrano/recipes/deploy/dependencies'
 
 module Capistrano
@@ -49,16 +50,24 @@ module Capistrano
           # A wrapper for Kernel#system that logs the command being executed.
           def system(*args)
             cmd = args.join(' ')
+            result = nil
             if RUBY_PLATFORM =~ /win32/
 							cmd = cmd.split(/\s+/).collect {|w| w.match(/^[\w+]+:\/\//) ? w : w.gsub('/', '\\') }.join(' ') # Split command by spaces, change / by \\ unless element is a some+thing:// 
               cmd.gsub!(/^cd /,'cd /D ') # Replace cd with cd /D
               cmd.gsub!(/&& cd /,'&& cd /D ') # Replace cd with cd /D
               logger.trace "executing locally: #{cmd}"
-              super(cmd)
+              elapsed = Benchmark.realtime do
+                result = super(cmd)
+              end
             else
               logger.trace "executing locally: #{cmd}"
-              super
+              elapsed = Benchmark.realtime do
+                result = super
+              end
             end
+            
+            logger.trace "command finished in #{(elapsed * 1000).round}ms"
+            result
           end
 
         private
