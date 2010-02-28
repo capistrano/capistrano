eb37051b0c65a5626893f6e99b0224da53542fb7
Lee Hambley <lee.hambley@gmail.com>
2010-02-28 16:29:57 +0100

    Version bump to 2.5.17

--- lib/capistrano/version.rb
+++ lib/capistrano/version.rb
@@ -1,8 +1,11 @@
+require 'scanf'
 module Capistrano
 
   class Version
 
     CURRENT = File.read(File.dirname(__FILE__) + '/../../VERSION')
+
+    MAJOR, MINOR, TINY = CURRENT.scanf('%d.%d.%d')
 
     STRING = CURRENT.to_s
 
