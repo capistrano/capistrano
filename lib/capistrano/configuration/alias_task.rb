297d8cbc39f884a03c57b4446ce590fc4a9d1b69
Rafa García <contacto@rafagarcia.net>
2011-05-08 17:53:30 +0200

    Ticket #193 - alias_task first implementation

---
+++ lib/capistrano/configuration/alias_task.rb
@@ -0,0 +1,19 @@
+module Capistrano
+  class Configuration
+    module AliasTask
+      # Attempts to find the task at the given fully-qualified path, and
+      # alias it. If arguments don't have correct task names, an ArgumentError
+      # wil be raised. If no such task exists, a Capistrano::NoSuchTaskError
+      # will be raised.
+      def alias_task(new_name, old_name)
+        if !new_name.respond_to?(:to_sym) or !old_name.respond_to?(:to_sym)
+          raise ArgumentError, "expected a valid task name"
+        end
+
+        task = find_task(old_name) or raise NoSuchTaskError, "the task `#{old_name}' does not exist"
+
+        task(new_name, task.options, &task.body)
+      end
+    end
+  end
+end
