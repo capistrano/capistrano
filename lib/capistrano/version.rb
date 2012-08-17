3632188f8f10265521265f5b7106dc4bcadcfcad
Roy Liu <carsomyr@gmail.com>
2012-08-17 17:08:48 -0400

    Bump Capistrano::Version to 2.13.0

--- lib/capistrano/version.rb
+++ lib/capistrano/version.rb
@@ -1,16 +1,11 @@
-require 'scanf'
 module Capistrano
-
   class Version
-
     MAJOR = 2
-    MINOR = 12
+    MINOR = 13
     PATCH = 0
 
     def self.to_s
       "#{MAJOR}.#{MINOR}.#{PATCH}"
     end
-
   end
-
 end
