capistrano/capistrano

commit f84b872377bbfc02b8036965e754e09e750f5c26

lib/capistrano/processable.rb: log output if nothing happens for a while
Currently, cap can hang indefinitely waiting for output from a server.
This adds a 10 second timeout to the IO.select command and prints the
names of the servers to which we still have open channels.  This allows
us to easily see which servers are taking a long time or hanging.

lib/capistrano/processable.rb
@@ -21,8 +21,18 @@ module Capistrano
       readers = sessions.map { |session| session.listeners.keys }.flatten.reject { |io| io.closed? }
       writers = readers.select { |io| io.respond_to?(:pending_write?) && io.pending_write? }
 
-      if readers.any? || writers.any?
-        readers, writers, = IO.select(readers, writers, nil, wait)
+      io_timeout = 10
+      loop do
+        if readers.any? || writers.any?
+          rs, ws, = IO.select(readers, writers, nil, io_timeout)
+          if rs.nil? && ws.nil?
+            logger.info("Waiting for #{@channels.select{ |ch| !ch[:closed] }.map { |ch| ch[:server] }.join(',')}")
+          else
+            readers = rs
+            writers = ws
+            break
+          end
+        end
       end
 
       if readers

commit b2600bc6a543c057e055e9c06fc4016a4262ff73

processable.rb: fix case where there are no readers/writers
This fixes a regression introduced in f84b872 whereby the
process_iteration function can loop forever if called with all
channels already marked as closed.

Also, change the wording when waiting on open channels.

lib/capistrano/processable.rb
@@ -22,11 +22,11 @@ module Capistrano
       writers = readers.select { |io| io.respond_to?(:pending_write?) && io.pending_write? }
 
       io_timeout = 10
-      loop do
-        if readers.any? || writers.any?
+      if readers.any? || writers.any?
+        loop do
           rs, ws, = IO.select(readers, writers, nil, io_timeout)
           if rs.nil? && ws.nil?
-            logger.info("Waiting for #{@channels.select{ |ch| !ch[:closed] }.map { |ch| ch[:server] }.join(',')}")
+            logger.info("Still waiting on #{@channels.select{ |ch| !ch[:closed] }.map { |ch| ch[:server] }.join(',')}")
           else
             readers = rs
             writers = ws