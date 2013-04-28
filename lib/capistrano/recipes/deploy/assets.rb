667c1c5f4430d33ee5f025a9e0f9565046fef2a8
Jim Ryan <jim@room118solutions.com>
2013-04-27 20:56:30 -0400

    Raise error when more than one asset manifest file is detected
    If a user is upgrading from Rails 3 to 4, the assets:precompile task will ignore the manifest.yml
    that was generated in Rails 3 and generate its own JSON manifest. It's impossible for us to know
    which manifest the user's current code requires, so it's important that we only have one manifest
    in the assets directory at a time. When we detect two manifests, we raise an error pointing the
    user towards a wiki entry explaining the problem and instructing them to remove the Rails 3.x manifest.

--- lib/capistrano/recipes/deploy/assets.rb
+++ lib/capistrano/recipes/deploy/assets.rb
@@ -64,6 +64,10 @@ namespace :deploy do
         #{rake} RAILS_ENV=#{rails_env.to_s.shellescape} #{asset_env} assets:precompile
       CMD
 
+      if capture("ls -1 #{shared_path.shellescape}/#{shared_assets_prefix}/manifest* | wc -l").to_i > 1
+        raise "More than one asset manifest file was found in '#{shared_path.shellescape}/#{shared_assets_prefix}'.  If you are upgrading a Rails 3 application to Rails 4, follow these instructions: http://github.com/capistrano/capistrano/wiki/Upgrading-to-Rails-4#asset-pipeline"
+      end
+
       # Sync manifest filenames across servers if our manifest has a random filename
       if shared_manifest_path =~ /manifest-.+\./
         run <<-CMD.compact
