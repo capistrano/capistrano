0bc03484db34d90152b2a13b0cae6a04f3efe6cb
Lee Hambley <lee.hambley@gmail.com>
2013-05-28 17:05:46 +0200

    Improved error messages for execution without stage

--- lib/capistrano/i18n.rb
+++ lib/capistrano/i18n.rb
@@ -1,31 +1,33 @@
 require 'i18n'
+
 en = {
-  starting: 'Starting',
-  capified: 'Capified',
-  starting: 'Starting',
-  start: 'Start',
-  update: 'Update',
-  finalize: 'Finalise',
-  restart: 'Restart',
-  finishing: 'Finishing',
-  finished: 'Finished',
-  stage_not_set: 'Stage not set',
-  written_file: 'create %{file}',
-  question: 'Please enter %{key}: |%{default_value}|',
-  keeping_releases: 'Keeping %{keep_releases} of %{releases} deployed releases',
-  linked_file_does_not_exist: 'linked file %{file} does not exist on %{host}',
-  mirror_exists: "The repository mirror is at %{at}",
-  revision_log_message: 'Branch %{branch} deployed as release %{release} by %{user}',
-  rollback_log_message: '%{user} rolled back to release %{release}',
+  starting:                    'Starting',
+  capified:                    'Capified',
+  starting:                    'Starting',
+  start:                       'Start',
+  update:                      'Update',
+  finalize:                    'Finalise',
+  restart:                     'Restart',
+  finishing:                   'Finishing',
+  finished:                    'Finished',
+  stage_not_set:               'Stage not set, please call something such as `cap production deploy`, where production is a stage you have defined.',
+  written_file:                'create %{file}',
+  question:                    'Please enter %{key}: |%{default_value}|',
+  keeping_releases:            'Keeping %{keep_releases} of %{releases} deployed releases',
+  linked_file_does_not_exist:  'linked file %{file} does not exist on %{host}',
+  mirror_exists:               "The repository mirror is at %{at}",
+  revision_log_message:        'Branch %{branch} deployed as release %{release} by %{user}',
+  rollback_log_message:        '%{user} rolled back to release %{release}',
   console: {
-    welcome: 'capistrano console - enter command to execute on %{stage}',
-    bye: 'bye'
+    welcome:  'capistrano console - enter command to execute on %{stage}',
+    bye:      'bye'
   },
   error: {
     user: {
-      does_not_exist: 'User %{user} does not exists',
-      cannot_switch: 'Cannot switch to user %{user}'
+      does_not_exist:  'User %{user} does not exists',
+      cannot_switch:   'Cannot switch to user %{user}'
     }
   }
 }
+
 I18n.backend.store_translations(:en, { capistrano: en })
