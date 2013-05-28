8276aeeaacf001ecaee8f4e36874ac3378b7b76f
Lee Hambley <lee.hambley@gmail.com>
2013-05-28 12:14:02 +0200

    Format for module style

--- spec/lib/capistrano/configuration/question_spec.rb
+++ spec/lib/capistrano/configuration/question_spec.rb
@@ -1,49 +1,54 @@
 require 'spec_helper'
 
 module Capistrano
-  describe Configuration::Question do
-    let(:question) { Configuration::Question.new(env, key, default) }
-    let(:default) { :default }
-    let(:key) { :branch }
-    let(:env) { stub }
+  class Configuration
 
-    describe '.new' do
-      it 'takes a key, default' do
-        question
+    describe Question do
+
+      let(:question) { Question.new(env, key, default) }
+      let(:default) { :default }
+      let(:key) { :branch }
+      let(:env) { stub }
+
+      describe '.new' do
+        it 'takes a key, default' do
+          question
+        end
+      end
+
+      describe '#call' do
+        subject { question.call }
+
+        context 'value is entered' do
+          let(:branch) { 'branch' }
+
+          before do
+            $stdout.expects(:puts).with('Please enter branch: |default|')
+            $stdin.expects(:gets).returns(branch)
+          end
+
+          it 'sets the value' do
+            env.expects(:set).with(key, branch)
+            question.call
+          end
+        end
+
+        context 'value is not entered' do
+          let(:branch) { default }
+
+          before do
+            $stdout.expects(:puts).with('Please enter branch: |default|')
+            $stdin.expects(:gets).returns('')
+          end
+
+          it 'sets the default as the value' do
+            env.expects(:set).with(key, branch)
+            question.call
+          end
+
+        end
       end
     end
 
-    describe '#call' do
-      subject { question.call }
-
-      context 'value is entered' do
-        let(:branch) { 'branch' }
-
-        before do
-          $stdout.expects(:puts).with('Please enter branch: |default|')
-          $stdin.expects(:gets).returns(branch)
-        end
-
-        it 'sets the value' do
-          env.expects(:set).with(key, branch)
-          question.call
-        end
-      end
-
-      context 'value is not entered' do
-        let(:branch) { default }
-
-        before do
-          $stdout.expects(:puts).with('Please enter branch: |default|')
-          $stdin.expects(:gets).returns('')
-        end
-
-        it 'sets the default as the value' do
-          env.expects(:set).with(key, branch)
-          question.call
-        end
-      end
-    end
   end
-
 end
