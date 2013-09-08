f040f12353c3ad8f682631463743691b785b3353
seenmyfate <seenmyfate@gmail.com>
2013-09-08 10:51:51 +0100

    Fix default configuration load order
    Ensure application specific configuration is loaded after
    `load:defaults`.  This closes #640 and
    https://github.com/capistrano/bundler/issues/5

--- lib/capistrano/setup.rb
+++ lib/capistrano/setup.rb
@@ -3,13 +3,13 @@ include Capistrano::DSL
 namespace :load do
   task :defaults do
     load 'capistrano/defaults.rb'
-    load 'config/deploy.rb'
   end
 end
 
 stages.each do |stage|
   Rake::Task.define_task(stage) do
     invoke 'load:defaults'
+    load 'config/deploy.rb'
     load "config/deploy/#{stage}.rb"
     load "capistrano/#{fetch(:scm)}.rb"
     set(:stage, stage.to_sym)
