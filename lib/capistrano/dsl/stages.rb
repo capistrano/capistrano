0fe381ae40d232217e6e78602bcba93a916b77d3
Andriy Yanko <andriy.yanko@gmail.com>
2013-10-18 02:50:00 +0300

    Support stages with nested configurations

--- lib/capistrano/dsl/stages.rb
+++ lib/capistrano/dsl/stages.rb
@@ -2,8 +2,47 @@ module Capistrano
   module DSL
     module Stages
 
+      def stages_root
+        File.expand_path(fetch(:stages_root, 'config/deploy'))
+      end
+
+      # Build stages with nested configurations
+      #
+      # @example simple stages
+      #
+      #   config
+      #   ├── deploy
+      #   │   ├── production.rb
+      #   │   └── staging.rb
+      #   └── deploy.rb
+      #
+      # * cap production
+      # * cap staging
+      #
+      # @example stages with nested configurations
+      #
+      #   config
+      #   ├── deploy
+      #   │   ├── soa
+      #   │   │   ├── blog
+      #   │   │   │   ├── production.rb
+      #   │   │   │   └── staging.rb
+      #   │   │   └── wiki
+      #   │   │       └── qa.rb
+      #   │   └── soa.rb
+      #   └── deploy.rb
+      #
+      # * cap soa:blog:production
+      # * cap soa:blog:staging
+      # * cap soa:wiki:qa
       def stages
-        Dir['config/deploy/*.rb'].map { |f| File.basename(f, '.rb') }
+        Dir["#{stages_root}/**/*.rb"].map { |file|
+          file.slice(stages_root.size + 1 .. -4).tr('/', ':')
+        }.tap { |paths|
+          paths.reject! { |path|
+            paths.any? { |another| another != path && another.start_with?(path) }
+          }
+        }.sort
       end
 
       def stage_set?
--- lib/capistrano/setup.rb
+++ lib/capistrano/setup.rb
@@ -8,15 +8,34 @@ end
 
 stages.each do |stage|
   Rake::Task.define_task(stage) do
-    set(:stage, stage.to_sym)
-    
+    set(:stage, stage)
+
     invoke 'load:defaults'
-    load 'config/deploy.rb'
-    load "config/deploy/#{stage}.rb"
+
+    # Load stage configuration(s).
+    #
+    # For stage 'production' will be loaded next configurations:
+    #
+    # * config/deploy.rb
+    # * config/deploy/production.rb
+    #
+    # For stage 'soa:blog:production' will be loaded next configurations:
+    #
+    # * config/deploy.rb
+    # * config/deploy/soa.rb
+    # * config/deploy/soa/blog.rb
+    # * config/deploy/soa/blog/production.rb
+    stage.split(':').inject([stages_root]) do |paths, segment|
+      paths << File.join(paths.last, segment)
+    end.each do |path|
+      file = "#{path}.rb"
+      load(file) if File.exists?(file)
+    end
+
     load "capistrano/#{fetch(:scm)}.rb"
     I18n.locale = fetch(:locale, :en)
     configure_backend
-  end
+  end.add_description("Load #{stage} configurations")
 end
 
 require 'capistrano/dotfile'
