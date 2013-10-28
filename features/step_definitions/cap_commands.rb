a20824e27888b2aaf9a50e0a3c9b8328336e582c
Clinton Blackburn <cblackburn@kyru.us>
2013-10-28 17:14:28 -0400

    Attempt at testing deploy:failed
    After hours of trial and error, I cannot get this to work properly. I commit it in hopes that someone may come along and fix it.

--- features/step_definitions/cap_commands.rb
+++ features/step_definitions/cap_commands.rb
@@ -6,3 +6,17 @@ When(/^I run cap "(.*?)" as part of a release$/) do |task|
   TestApp.cap("deploy:new_release_path #{task}")
 end
 
+When(/^I run cap "(.*?)" and it fails$/) do |task|
+  TestApp.copy_task_to_test_app('spec/support/tasks/deploy_failure.cap')
+  TestApp.cap(task)
+end
+
+Then(/^deploy:failed is invoked$/) do
+  Dir.chdir(TestApp.task_dir) do
+    file = 'FAILED'
+
+    output = %x[#{test_file_exists(file)}]
+    %x[rm -f #{file}]
+    expect(output).to match(/exists\.$/)
+  end
+end
