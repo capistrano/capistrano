e996b33da0f0a03bce0dbcb625f9c0a0fc37b80e
Hartog C. de Mik <hartog@organisedminds.com>
2013-12-09 21:50:27 +0100

    Decapsulated git rake code

--- lib/capistrano/git.rb
+++ lib/capistrano/git.rb
@@ -1,1 +1,25 @@
 load File.expand_path("../tasks/git.rake", __FILE__)
+
+module Capistrano::Git
+  class << self
+    def test(context)
+      context.test " [ -f #{context.repo_path}/HEAD ] "
+    end
+
+    def check(context)
+      exit 1 unless context.test :git, :'ls-remote', context.repo_url
+    end
+
+    def clone(context)
+      context.execute :git, :clone, '--mirror', context.repo_url, context.repo_path
+    end
+
+    def update(context)
+      context.execute :git, :remote, :update
+    end
+
+    def release(context)
+      context.execute :git, :archive, context.fetch(:branch), '| tar -x -C', context.release_path
+    end
+  end
+end
