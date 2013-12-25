6a378200c53ebd7056faa762ee71dff9cb898f76
Bryan Ricker <bricker88@gmail.com>
2013-12-25 02:40:46 -0800

    :scissors: trailing whitespace

--- lib/capistrano/setup.rb
+++ lib/capistrano/setup.rb
@@ -9,7 +9,7 @@ end
 stages.each do |stage|
   Rake::Task.define_task(stage) do
     set(:stage, stage.to_sym)
-    
+
     invoke 'load:defaults'
     load deploy_config_path
     load stage_config_path.join("#{stage}.rb")
