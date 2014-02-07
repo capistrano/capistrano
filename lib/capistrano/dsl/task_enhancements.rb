76e180f4686128385f1b7c8a22cae668013cd50c
Michael Nikitochkin <miry.sof@gmail.com>
2014-02-07 15:16:13 +0200

    :panda_face: Added ability to add args to callabcks after and before.

--- lib/capistrano/dsl/task_enhancements.rb
+++ lib/capistrano/dsl/task_enhancements.rb
@@ -2,14 +2,16 @@ module Capistrano
   module TaskEnhancements
     def before(task, prerequisite, *args, &block)
       prerequisite = Rake::Task.define_task(prerequisite, *args, &block) if block_given?
-      Rake::Task[task].enhance [prerequisite]
+      Rake::Task[task].enhance do
+        Rake::Task[prerequisite].invoke(*args)
+      end
     end
 
     def after(task, post_task, *args, &block)
       Rake::Task.define_task(post_task, *args, &block) if block_given?
       post_task = Rake::Task[post_task]
       Rake::Task[task].enhance do
-        post_task.invoke
+        post_task.invoke(args)
       end
     end
 
