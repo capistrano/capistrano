b586c2d28e67d65e3c81ac1832d9a8aafc7b5395
Lee Hambley <lee.hambley@gmail.com>
2014-04-22 08:19:26 +0200

    Revert "Merge pull request #928 from miry/before_with_args"
    This reverts commit 55bf5763721906857b7f1b2e141d2645ed1f9193, reversing
    changes made to 02b847e040abaeacf972545c088444d9705454c0.

--- lib/capistrano/dsl/task_enhancements.rb
+++ lib/capistrano/dsl/task_enhancements.rb
@@ -2,16 +2,14 @@ module Capistrano
   module TaskEnhancements
     def before(task, prerequisite, *args, &block)
       prerequisite = Rake::Task.define_task(prerequisite, *args, &block) if block_given?
-      Rake::Task[task].enhance do
-        Rake::Task[prerequisite].invoke(*args)
-      end
+      Rake::Task[task].enhance [prerequisite]
     end
 
     def after(task, post_task, *args, &block)
       Rake::Task.define_task(post_task, *args, &block) if block_given?
       post_task = Rake::Task[post_task]
       Rake::Task[task].enhance do
-        post_task.invoke(args)
+        post_task.invoke
       end
     end
 
