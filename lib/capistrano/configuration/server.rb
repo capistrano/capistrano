281f2cbe42c1a599c4fcf8f03ff2277fac416d76
Nick Townsend <nick.townsend@mac.com>
2014-09-24 20:14:02 -0700

    Override netssh_options and use super
    The existing method of using aliases is confusing
    and unnecessary

--- lib/capistrano/configuration/server.rb
+++ lib/capistrano/configuration/server.rb
@@ -42,11 +42,9 @@ module Capistrano
         @properties ||= Properties.new
       end
 
-      def netssh_options_with_options
-        @netssh_options ||= netssh_options_without_options.merge( fetch(:ssh_options) || {} )
+      def netssh_options
+        @netssh_options ||= super.merge( fetch(:ssh_options) || {} )
       end
-      alias_method :netssh_options_without_options, :netssh_options
-      alias_method :netssh_options, :netssh_options_with_options
 
       def roles_array
         roles.to_a
