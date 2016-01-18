08331edd0e4323a7557466dbca3ad4117d555ae2
Matt Brictson <matt@mattbrictson.com>
2016-01-17 17:47:52 -0800

    Fix intermittent Travis failures

--- spec/lib/capistrano/immutable_task_spec.rb
+++ spec/lib/capistrano/immutable_task_spec.rb
@@ -4,6 +4,11 @@ require 'capistrano/immutable_task'
 
 module Capistrano
   describe ImmutableTask do
+    after do
+      # Ensure that any tasks we create in these tests don't pollute other tests
+      Rake::Task.clear
+    end
+
     it 'prints warning and raises when task is enhanced' do
       extend(Rake::DSL)
 
