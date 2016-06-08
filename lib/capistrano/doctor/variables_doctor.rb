e1158dcf3a15a45b88333361bcf36c76fa7155a7
ShaneSaww <shanesahnow@gmail.com>
2016-06-08 11:37:18 -0700

    Updating the variables in the VariblesDoctor WHITELIST.

--- lib/capistrano/doctor/variables_doctor.rb
+++ lib/capistrano/doctor/variables_doctor.rb
@@ -7,7 +7,7 @@ module Capistrano
     class VariablesDoctor
       # These are keys that have no default values in Capistrano, but are
       # nonetheless expected to be set.
-      WHITELIST = [:application, :repo_url].freeze
+      WHITELIST = [:application, :repo_url, :git_strategy, :hg_strategy, :svn_strategy].freeze
       private_constant :WHITELIST
 
       include Capistrano::Doctor::OutputHelpers
