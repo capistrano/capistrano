800d84065fcf2f5b12f0294c1b97e4d763a0c988
Eric Guo <eric@cloud-mes.com>
2020-05-10 08:20:19 +0800

    #2060 Fix ruby 2.7 keyword parameters warnings and bump up ruby versions. (#2060)

--- lib/capistrano/dsl.rb
+++ lib/capistrano/dsl.rb
@@ -33,7 +33,7 @@ module Capistrano
     end
 
     def t(key, options={})
-      I18n.t(key, options.merge(scope: :capistrano))
+      I18n.t(key, **options.merge(scope: :capistrano))
     end
 
     def scm
